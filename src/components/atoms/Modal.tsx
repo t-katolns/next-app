70665ac3fc58f37961a24a1a7e1430b65c7f97e2
Ttt Iii <tk.katotai11@gmail.com>
2020-09-02 00:01:45 +0900

    modalComponents追加

--- src/components/atoms/Modal.tsx
+++ src/components/atoms/Modal.tsx
@@ -5,7 +5,7 @@ const MODAL_STYLE = {
   position: "fixed",
   top: "50%",
   left: "50%",
-  transform: "trancelate(50%, 50%)",
+  transform: "translate(-50%, -50%)",
   backgroundColor: "#FFF",
   padding: "50px",
   zIndex: 1000,
@@ -16,7 +16,8 @@ const OVERLAY_STYLES = {
   top: 0,
   left: 0,
   right: 0,
-  backgroundColor: "blue",
+  bottom: 0,
+  backgroundColor: "rgba(109, 123, 143, 0.7)",
   zIndex: 1000,
 };
 
